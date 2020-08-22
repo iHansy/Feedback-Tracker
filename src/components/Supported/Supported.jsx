2331b301b910d67915ba5930ddc411e969bd6f6b
Hans Accola <Hansolo6283@gmail.com>
2020-08-22 11:53:52 -0500

    added dispatch and history push in supported next btn

--- src/components/Supported/Supported.jsx
+++ src/components/Supported/Supported.jsx
@@ -3,6 +3,22 @@ import { connect } from 'react-redux';
 
 class Supported extends Component {
 
+    state = {
+        supported: ''
+    }
+
+    supportedInput = (event) => {
+        this.setState({
+            supported: event.target.value
+        })
+    }
+
+    supportedNextBtn = () => {
+        console.log(this.state);
+        this.props.dispatch({ type: 'SET_SUPPORTED', payload: this.state.supported})
+        this.props.history.push('/comments')
+    }
+
 
 
     render() {
@@ -11,7 +27,7 @@ class Supported extends Component {
                 <h1>How well are you feeling supported?</h1>
                 <p>1 = not feeling supported at all</p>
                 <p>5 = feeling very supported</p>
-                <input type="number" placeholder="enter number" onChange={this.supportedInput}/>
+                <input type="number" placeholder="enter number" onChange={this.supportedInput} />
                 <button onClick={this.supportedNextBtn}>Next</button>
             </div>
         )
