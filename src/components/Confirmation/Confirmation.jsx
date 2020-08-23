6a72e7775e4b5f6560675fc981125e7e29269cbd
Hans Accola <Hansolo6283@gmail.com>
2020-08-23 00:29:12 -0500

    completed base mode, reducers get reset

--- src/components/Confirmation/Confirmation.jsx
+++ src/components/Confirmation/Confirmation.jsx
@@ -7,6 +7,7 @@ class Confirmation extends Component {
     confirmationHomeBtn = () => {
         console.log('returning home...')
         console.log(this.props.reduxStore);
+        this.props.dispatch({type: 'RESET', payload: {}}) //resetting all reducer values when returning home
         this.props.history.push('/'); //changing url to home
     }
 
